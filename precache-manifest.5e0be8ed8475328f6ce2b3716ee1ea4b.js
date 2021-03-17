7e57ca6d9e58d5d9ee7e31f741f769b8288bc7cd
juanignacioluna <juluna@frba.utn.edu.ar>
2021-03-17 02:17:31 -0300

    Updates

---
+++ precache-manifest.5e0be8ed8475328f6ce2b3716ee1ea4b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9fa2af59626c0b178265c54db71c697",
+    "url": "/desafio-esto-es-front-deploy/index.html"
+  },
+  {
+    "revision": "683e0f99885b7e09936e",
+    "url": "/desafio-esto-es-front-deploy/static/css/main.d73533d8.chunk.css"
+  },
+  {
+    "revision": "acf6652bb627b1f68138",
+    "url": "/desafio-esto-es-front-deploy/static/js/2.a921f4cd.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/desafio-esto-es-front-deploy/static/js/2.a921f4cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "683e0f99885b7e09936e",
+    "url": "/desafio-esto-es-front-deploy/static/js/main.fdf95c14.chunk.js"
+  },
+  {
+    "revision": "521c1969ac2e5ac54786",
+    "url": "/desafio-esto-es-front-deploy/static/js/runtime-main.1039910f.js"
+  },
+  {
+    "revision": "60dbe62ecd94409f911f948ce2b0119c",
+    "url": "/desafio-esto-es-front-deploy/static/media/messi.60dbe62e.jpg"
+  }
+]);
